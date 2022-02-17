2db3bb1692886e0f401bde786e92d5ebed2ef560
Shinhoo Lee <tlsgn511@icloud.com>
2022-02-17 15:13:49 +0900

    django course 36 commit

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
+#
+#
+class Article(models.Model):
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
 
-# Create your models here.
+    title = models.CharField(max_length=200, null=True)
+    image = models.ImageField(upload_to='article/', null=False)
+    content = models.TextField(null=True)
+
+    created_at = models.DateField(auto_created=True, null=True)
